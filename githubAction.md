9d7ba75654a6cfae3933ac5bbe39a755fe32b58d
wintjade <winterxuc@gmail.com>
2021-02-14 15:26:38 +0800

    udate un_walk_repo

--- githubAction.md
+++ githubAction.md
@@ -1,4 +1,5 @@
 # https://github.com/Squaregentleman/mimotion
+# https://github.com/FKPYW/mimotion
 
 # 小米运动自动刷步数
 
@@ -10,10 +11,12 @@
 
 ### 二、设置账号密码
 
-添加名为  **SCKEY**、**USER**、**PWD**、**STEP** 的变量，值分别为 **server酱推送key（0关闭）**、 **账号（仅支持手机号）**、**密码**、**步数（0则为1w-2w之间随机 或自定义随机范围[18000-25000]）**
+添加名为  **XMYD_USER**、**XMYD_PASSWD**、**XMYD_STEP**、**TG_BOT_TOKEN**、**TG_USER_ID** 的变量，值分别为 、 **账号（仅支持手机号）**、**密码**、**步数（0则为1w-2w之间随机 或自定义随机范围[18000-25000]）**
+
+推送为TG推送
 
 > Settings-->Secrets-->New secret
 
-## 注意事项
+### 三、多账户(用不上请忽略)
 
-1. 每天运行一次，在下午 6 点
+多账户请用 **#** 分割
--- main.py
+++ main.py
@@ -2,10 +2,31 @@
 # python >=3.8
 
 import requests,time,re,json,random
+import os
+
+TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]           # telegram bot token 自行申请
+TG_USER_ID = os.environ["TG_USER_ID"]             # telegram 用户ID
+
+def telegram_bot(title, content):
+    print("\n")
+    tg_bot_token = TG_BOT_TOKEN
+    tg_user_id = TG_USER_ID
+    if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
+        tg_bot_token = os.environ["TG_BOT_TOKEN"]
+        tg_user_id = os.environ["TG_USER_ID"]
+    if not tg_bot_token or not tg_user_id:
+        print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
+        return
+    print("Telegram 推送开始")
+    send_data = {"chat_id": tg_user_id, "text": title +
+                 '\n\n'+content, "disable_web_page_preview": "true"}
+    response = requests.post(
+        url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data)
+    print(response.text)
 
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 headers = {
-        'User-Agent':'Mozilla/5.0 (Linux; U; Android 9; zh-cn; MI 6 Build/PKQ1.190118.001)'
+        'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
         }
  
 #获取登录code
@@ -33,7 +54,7 @@ def login(user,password):
         code = get_code(location)
     except:
         return 0,0
-    #print("access_code获取成功！")
+    #print("access_code获取成功！")ste
     #print(code)
      
     url2 = "https://account.huami.com/v2/client/login"
@@ -58,7 +79,7 @@ def login(user,password):
     return login_token,userid
  
 #主函数
-def main(user, passwd, step, sckey):
+def main(user, passwd, step):
     user = str(user)
     password = str(passwd)
     step = str(step)
@@ -67,8 +88,8 @@ def main(user, passwd, step, sckey):
         return
     
     if step == '':
-        print ("已设置为随机步数（16000-15000）")
-        step = str(random.randint(19000,25000))
+        print ("已设置为随机步数（10000-19999）")
+        step = str(random.randint(10000,19999))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
@@ -98,9 +119,8 @@ def main(user, passwd, step, sckey):
     
     response = requests.post(url, data=data, headers=head).json()
     #print(response)
-    result = f"[{now}] 修改步数（{step}）"+ response['message']
+    result = f"{user[:4]}****{user[-4:]}: [{now}] 修改步数（{step}）"+ response['message']
     print(result)
-    push_wx(sckey, result)
     return result
   
 #获取时间戳
@@ -118,44 +138,28 @@ def get_app_token(login_token):
     #print("app_token获取成功！")
     #print(app_token)
     return app_token
-    
-# 推送server
-def push_wx(sckey, desp=""):
-    """
-    推送消息到微信
-    """
-    if sckey == '':
-        print("[注意] 未提供sckey，不进行推送！")
-    else:
-        server_url = f"https://sc.ftqq.com/{sckey}.send"
-        params = {
-            "text": '小米运动 步数修改',
-            "desp": desp
-        }
- 
-        response = requests.get(server_url, params=params)
-        json_data = response.json()
- 
-        if json_data['errno'] == 0:
-            print(f"[{now}] 推送成功。")
-        else:
-            print(f"[{now}] 推送失败：{json_data['errno']}({json_data['errmsg']})")
 
 if __name__ ==  "__main__":
-    # ServerChan
-    sckey = input()
-    if str(sckey) == '0':
-        sckey = ''
     # 用户名（格式为 13800138000）
-    user = input()
+    user = os.environ["XMYD_USER"]
     # 登录密码
-    passwd = input()
+    passwd = os.environ["XMYD_PASSWD"]
     # 要修改的步数，直接输入想要修改的步数值，留空为随机步数
-    step = input()
+    step = os.environ["XMYD_STEP"]
+
+    user_list = user.split('#')
+    passwd_list = passwd.split('#')
     setp_array = step.split('-')
-    if len(setp_array) == 2:
-        step = str(random.randint(int(setp_array[0]),int(setp_array[1])))
-    elif str(step) == '0':
-        step = ''
-    main(user, passwd, step, sckey)
+
+    if len(user_list) == len(passwd_list):
+        push = ''
+        for line in range(0,len(user_list)):
+            if len(setp_array) == 2:
+                step = str(random.randint(int(setp_array[0]),int(setp_array[1])))
+            elif str(step) == '0':
+                step = ''
+            push += main(user_list[line], passwd_list[line], step) + '\n'
+        telegram_bot("小米运动", push)
+    else:
+        print('用户名和密码数量不对')
     
